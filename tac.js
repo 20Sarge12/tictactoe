8461429999ca696761b5243fedf243c06a4a7dcb
mohammad akbar <akbardastgeer@gmail.com>
2021-06-12 17:28:55 +0530

    changes

--- tac.js
+++ tac.js
@@ -1,8 +1,7 @@
 var matrixJs = [[3,3,3],[3,3,3],[3,3,3]];
 console.log(matrixJs);
 
-function btnClick(id){
+function btnClick(event){
+    var id = (event.target.id+"").split("btn")[1];
     console.log(id);
-    // var id = (event.target.id+"").split("btn")[1];
-    // console.log(id);
 }
