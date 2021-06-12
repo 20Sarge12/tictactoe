3617fa7d6bf25dde10109a0fe42d70a63d793fb0
20Sarge12 <mohammadayan1002@gmail.com>
2021-06-12 23:53:38 +0530

    changes

--- tac.js
+++ tac.js
@@ -1,9 +1,23 @@
 var matrixJs = [[3,3,3],[3,3,3],[3,3,3]];
 console.log(matrixJs);
 
+window.onload = funcStart;
+
 function btnClick(event){
     var id = (event.target.id+"").split("btn")[1];
     console.log(id);
     matrixJs[id.split("")[0]][id.split("")[1]]= 0;
     console.log(matrixJs);
+   
 }
+function funcStart(){
+    var p1 = window.prompt("Enter player1's name");
+    var p2 = window.prompt("Enter player2's name");
+     document.querySelector(".player1Div p").textContent = p1;
+     document.querySelector(".player2Div p").textContent = p2;
+     var player1Sign = Math.floor(Math.random()*2);
+     var player2Sign = player1Sign?0:1;
+     document.querySelector(".player1Sign").textContent = player1Sign;
+     document.querySelector(".player2Sign").textContent = player2Sign;
+}
+
