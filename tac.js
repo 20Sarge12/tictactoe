75131f51a1bf820535dace407175572552eb4ca8
20Sarge12 <mohammadayan1002@gmail.com>
2021-06-12 17:36:27 +0530

    chabgdfdf

--- tac.js
+++ tac.js
@@ -4,4 +4,7 @@ console.log(matrixJs);
 function btnClick(event){
     var id = (event.target.id+"").split("btn")[1];
     console.log(id);
+    var p =id.split("");
+    matrixJs[p[0]][p[1]]= 0;
+    console.log(matrixJs);
 }
