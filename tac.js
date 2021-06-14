61c1e5ca3da008088741562432be8312dba915a9
mohammad akbar <akbardastgeer@gmail.com>
2021-06-14 11:52:12 +0530

    activePlayerCode added

--- tac.js
+++ tac.js
@@ -1,23 +1,43 @@
 var matrixJs = [[3,3,3],[3,3,3],[3,3,3]];
-console.log(matrixJs);
+
+var player1Obj={
+    player1Name:"",
+    player1Sign:6
+}
+var player2Obj={
+    player2Name:"",
+    player2Sign:6
+}
 
 window.onload = funcStart;
 
+var activePlayer ={
+} 
+
 function btnClick(event){
+    document.querySelector(`#${event.target.id}`).textContent = activePlayer[Object.keys(activePlayer)[1]];
     var id = (event.target.id+"").split("btn")[1];
     console.log(id);
     matrixJs[id.split("")[0]][id.split("")[1]]= 0;
     console.log(matrixJs);
-   
+    //changeActivePlayerFunction();
 }
+
+function changeActivePlayerFunction(){
+
+}
+
 function funcStart(){
-    var p1 = window.prompt("Enter player1's name");
-    var p2 = window.prompt("Enter player2's name");
-     document.querySelector(".player1Div p").textContent = p1;
-     document.querySelector(".player2Div p").textContent = p2;
-     var player1Sign = Math.floor(Math.random()*2);
-     var player2Sign = player1Sign?0:1;
-     document.querySelector(".player1Sign").textContent = player1Sign;
-     document.querySelector(".player2Sign").textContent = player2Sign;
+     player1Obj.player1Name = window.prompt("Enter player1's name");
+     player2Obj.player2Name = window.prompt("Enter player2's name");
+     document.querySelector(".player1Div p").textContent = player1Obj.player1Name;
+     document.querySelector(".player2Div p").textContent = player2Obj.player2Name;
+     player1Obj.player1Sign = Math.floor(Math.random()*2);
+     player2Obj.player2Sign = player1Obj.player1Sign?0:1;
+     document.querySelector(".player1Sign").textContent = player1Obj.player1Sign;
+     document.querySelector(".player2Sign").textContent = player2Obj.player2Sign;
+     Object.assign(activePlayer,(Math.floor(Math.random()*2) == player1Obj.player1Sign)?player1Obj:player2Obj);
+     console.log(`activeplayer: ${activePlayer[Object.keys(activePlayer)[0]]}`);
+     document.querySelector(".playerTurn").textContent = activePlayer[Object.keys(activePlayer)[0]];
 }
 
